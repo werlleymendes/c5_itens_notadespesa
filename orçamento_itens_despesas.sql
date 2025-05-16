059cfbb5e0deff195ea7803dbe99cf3b3b31e757
Werley Mendes Andrade Maciel <werlleymendes@hotmail.com>
2025-05-16 15:58:11 -0300

    altera filtro da consulta de = para like

--- orçamento_itens_despesas.sql
+++ orçamento_itens_despesas.sql
@@ -6,7 +6,7 @@ SELECT c.seqpessoa, c.fantasia fornecedor, b.codproduto, b.descricao, sum(b.quan
             ON a.seqpessoa = c.seqpessoa
        JOIN consinco.ge_empresa d
             ON a.nroempresa = d.nroempresa
-       WHERE a.seqpessoa || ' ' || c.fantasia = '18781 EXPONENCIAL ENERGIA LTDA' and
+       WHERE a.seqpessoa || ' ' || c.fantasia LIKE '18781 EXPONENCIAL ENERGIA LTDA' and
              a.dtaentrada between '01-may-2025' and '15-may-2025' and
              d.nomereduzido = '004-FILIAL'
        GROUP BY c.seqpessoa, c.fantasia, b.codproduto, b.descricao;
